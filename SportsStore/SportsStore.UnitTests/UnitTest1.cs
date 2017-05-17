a7450dbc3e9ab52ef7b8f4a1bf0a4842bc5f0e84
BadDavis <davis693@o2.pl.com>
2017-05-17 14:08:42 +0200

    pagination

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.HtmlHelpers;
 
 namespace SportsStore.UnitTests
 {
@@ -7,8 +16,88 @@ namespace SportsStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            //arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductId =2 , Name= "P1" },
+                new Product {ProductId = 3, Name = "P2" },
+                new Product {ProductId = 5, Name = "P3" },
+                new Product {ProductId = 6, Name = "P4" },
+                new Product {ProductId = 7, Name = "P5" }
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //act
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+
+            //assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+
+            // arrange - definiowanie metody pomocniczej HTML — potrzebujemy tego,
+            // aby użyć metody rozszerzającej
+            HtmlHelper myHelper = null;
+
+            // arrange - tworzenie danych PagingInfo
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            // arrange - konfigurowanie delegatu z użyciem wyrażenia lambda
+            Func<int, string> pageUrlDelegate = i => "Strona" + i;
+
+            // act
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
+
+            // assert
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Strona1"">1</a>"
+                + @"<a class=""btn btn-default btn-primary selected"" href=""Strona2"">2</a>"
+                 + @"<a class=""btn btn-default"" href=""Strona3"">3</a>",
+                 result.ToString());
+        }
+
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            //arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m=>m.Products).Returns(new Product[]
+            {
+                new Product {ProductId =2 , Name= "P1" },
+                new Product {ProductId = 3, Name = "P2" },
+                new Product {ProductId = 5, Name = "P3" },
+                new Product {ProductId = 6, Name = "P4" },
+                new Product {ProductId = 7, Name = "P5" }
+            });
+
+            //arrange
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //act
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+
+            //assert
+            PagingInfo info = result.PagingInfo;
+            Assert.AreEqual(info.CurrentPage, 2);
+            Assert.AreEqual(info.ItemsPerPage, 3);
+            Assert.AreEqual(info.TotalItems, 5);
+            Assert.AreEqual(info.TotalPages, 2);
         }
     }
 }
