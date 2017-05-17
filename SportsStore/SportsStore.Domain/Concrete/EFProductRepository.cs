293eb0176f1bb2b24283d63583f529601be5e4b2
BadDavis <davis693@o2.pl.com>
2017-05-17 12:26:18 +0200

    DI, database, controller

---
+++ SportsStore/SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.Domain
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDbContext _context = new EFDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                return _context.Products;
+            }
+        }
+    }
+}
---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+using Moq;
+using SportsStore.Domain;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            _kernel = kernel;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            _kernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+    }
+}
